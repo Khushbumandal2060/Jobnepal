7379a2a0f13f3a213778e85ce0c7254535235711
khushi <khusbumandal2060@gmail.com>
2026-06-30 06:59:45 +0545

    updated

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>JOBNepal - Job Portal</title>
+    <title>/ - Job Portal</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="assets/css/styles.css">
     <link rel="stylesheet" href="assets/css/top.css">
